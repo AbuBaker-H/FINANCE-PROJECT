c9b3773e20fc89034abb931625fbcefe3d865fa5
AbuBaker-H <59665794+AbuBaker-H@users.noreply.github.com>
2020-01-09 00:47:23 +0500

    added one heading tag

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       
          <h3>Author : Muhammad Abu baker</h3> 
+    <h1>Another Contributer</h1>
          <h3>Project: ICAMS_UR</h3>
       </header>
     </div>
